5f6a0a4b47c1bca447bf71bddfdcbb36b7df88f5
j1egeng <cyl021226@gmail.com>
2026-08-20 15:06:04 +0800

    fix workshop 7 security workflows

--- scripts/zap-script.sh
+++ scripts/zap-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker pull zaproxy/zap-stable
-docker run -i zaproxy/zap-stable zap-baseline.py -t "https://kenken64.github.io/bitcoin-order-app/" -l PASS > zap_baseline_report.html
+docker run --rm -i zaproxy/zap-stable zap-baseline.py -t "https://j1egeng.github.io/bitcoin-order-app/" -l PASS > zap_baseline_report.html
 
 echo $? > /dev/null
